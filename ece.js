8e7fbe7f23ba33470db58dda3a2a9a6077954c40
zeynepecekiris <zeynep.ece933@gmail.com>
2024-07-07 16:03:57 +0300

    first commit

---
+++ ece.js
@@ -0,0 +1,14 @@
+const ad = "can";
+
+//arrow function
+
+const can = () => "Can"
+
+let sonuc1 = can();
+console.log(sonuc1);
+
+//methodlar
+
+let sonuc2 = ad.toUpperCase();
+
+console.log(sonuc2);
